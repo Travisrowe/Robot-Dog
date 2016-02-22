3eaaccd66d700aa4f6c853d0bd4ab7e4a1b0d662
Travisrowe <travisrowe18@gmail.com>
2016-02-22 09:38:43 -0600

    Create TRRobotDog.py

---
+++ TRRobotDog.py
@@ -0,0 +1,17 @@
+'''
+Hastily added code...
+Note to self: need refresher on how to upload to GitHub from the command prompt.
+'''
+'''
+Travis Rowe
+Robot Dog
+This code may consist of adding existing code from other sources.
+Sources:
+https://pypi.python.org/pypi/SpeechRecognition/
+
+
+'''
+
+pip install SpeechRecognition #Library for performing speech recognition with support for
+							  #CMU Sphinx, Google Speech Recognition, Wit.ai, IBM Speech to Text, and
+							  #AT&T Speech to Text https://pypi.python.org/pypi/SpeechRecognition/
